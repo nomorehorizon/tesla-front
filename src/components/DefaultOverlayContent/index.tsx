a012fe2102b3992c86d15363068ee72a31251064
Lucas <lmachado72@outlook.com>
2021-02-10 18:53:40 -0300

    Started Styles

--- src/components/DefaultOverlayContent/index.tsx
+++ src/components/DefaultOverlayContent/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Heading, Buttons } from './styles';
 
 interface Props {
   label: string
@@ -17,6 +17,11 @@ const DefaultOverlayContent: React.FC<Props> = (
         <h1>{label}</h1>
         <h2>{description}</h2>
       </Heading>
+
+      <Buttons>
+        <button>Custom Order</button>
+        <button className="white">Existing Inventory</button>
+      </Buttons>
     </Container>
   );
 }
--- src/components/DefaultOverlayContent/styles.ts
+++ src/components/DefaultOverlayContent/styles.ts
@@ -3,3 +3,11 @@ import styled from 'styled-components';
 export const Container = styled.div`
   
 `;
+
+export const Heading = styled.div`
+
+`;
+
+export const Buttons = styled.div`
+
+`;
