fc053a557165eeb90c98d33b54b06d43920ba441
Lucas <lmachado72@outlook.com>
2021-02-10 19:22:53 -0300

    ModelSection Props

--- src/components/Model/ModelSection/index.tsx
+++ src/components/Model/ModelSection/index.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 import { Container } from './styles';
 
-const ModelSection: React.FC = () => {
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  modelName: string
+  overlayNode: React.ReactNode
+}
+
+const ModelSection: React.FC<Props> = ({ 
+  modelName, 
+  overlayNode, 
+  children, 
+  ...props 
+}) => {
   return (
-    <Container>
-      
+    <Container {...props}>
+      {children}
     </Container>
   );
 }
