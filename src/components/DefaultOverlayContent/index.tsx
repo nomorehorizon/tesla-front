2beb86fb16d0d79cacd9255994a3401d4da46da7
Lucas <lmachado72@outlook.com>
2021-02-10 18:49:35 -0300

    Started DefaultOverlayContent component

---
+++ src/components/DefaultOverlayContent/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+interface Props {
+  label: string
+  description: string
+}
+
+const DefaultOverlayContent: React.FC<Props> = (
+  label, 
+  description
+) => {
+  return (
+    <Container>
+      <Heading>
+        <h1>{label}</h1>
+        <h2>{description}</h2>
+      </Heading>
+    </Container>
+  );
+}
+
+export default DefaultOverlayContent;
