9d6a6419e0e79476a1bc195c269b3427e28cb165
Lucas <lmachado72@outlook.com>
2021-02-10 19:28:02 -0300

    Mapped ModelSection in Page component

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
 
+import { ModelSection, ModelsWrapper } from '../Model';
 import DefaultOverlayContent from '../DefaultOverlayContent';
 
 import { Container } from './styles';
+
 
 const Page: React.FC = () => {
   return (
     <Container>
       <ModelsWrapper>
         <div>
-          <ModelSection 
-            modelName="Model One"
-            overlayNode={
-              <DefaultOverlayContent 
-                label="Model One"
-                description="Order Online for Delivery"
-              />
-            }
-          />
+          {[
+            'Model One',
+            'Model Two',
+            'Model Three',
+            'Model Four',
+            'Model Five',
+            'Model Six',
+            'Model Seven',
+          ].map(modelName => (
+            <ModelSection 
+            key={modelName} // Todo elemento usando map precisa de key
+              className="colored"
+              modelName={modelName}
+              overlayNode={
+                <DefaultOverlayContent 
+                  label="Model One"
+                  description="Order Online for Delivery"
+                />
+              }
+            />
+          ))}
+
         </div>
       </ModelsWrapper>
     </Container>
