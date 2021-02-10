e01de30c919ed9a2cf47a12b7dcd1dad0a8c2146
Lucas <lmachado72@outlook.com>
2021-02-10 19:08:24 -0300

    Linked ModelsWrapper & Modelsection to Page

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import DefaultOverlayContent from '../DefaultOverlayContent';
+
 import { Container } from './styles';
 
 const Page: React.FC = () => {
   return (
     <Container>
-      <h1>Page</h1>
+      <ModelsWrapper>
+        <div>
+          <ModelSection 
+            modelName="Model One"
+            overlayNode={
+              <DefaultOverlayContent 
+                label="Model One"
+                description="Order Online for Delivery"
+              />
+            }
+          />
+        </div>
+      </ModelsWrapper>
     </Container>
   );
 }
